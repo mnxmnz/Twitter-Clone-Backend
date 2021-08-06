555658caaf831359323a31ee513d6b7486b0f0ca
mnxmnz <codi0703@naver.com>
2021-08-06 19:06:36 +0900

    feat: express post delete 기능 생성

--- app.ts
+++ app.ts
@@ -3,7 +3,27 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('hello express');
+  res.send('Hello express');
+});
+
+app.get('/api', (req, res) => {
+  res.send('Hello api');
+});
+
+app.get('/api/posts', (req, res) => {
+  res.json([
+    { id: 1, content: 'Hello 1' },
+    { id: 2, content: 'Hello 2' },
+    { id: 3, content: 'Hello 3' },
+  ]);
+});
+
+app.post('/api/post', (req, res) => {
+  res.json({ id: 1, content: 'Hello 1' });
+});
+
+app.delete('/api/post', (req, res) => {
+  res.json({ id: 1 });
 });
 
 app.listen(3065, () => {
